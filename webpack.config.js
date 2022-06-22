179452e76cf079018725b82c4ddf9747d24d2b70
Tang <istangxy@163.com>
2022-06-22 21:21:54 +0800

    完成了html转译

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 module.exports = {
     mode: 'development',//或者production  开发中用development，项目发布时用production
@@ -9,6 +9,10 @@ module.exports = {
         //根据内容产生一个hash,内容变了会得到一个新的文件
         filename: '[name].[contenthash].js'
     },
-    // plugins: [new HtmlWebpackPlugin()],
+    plugins: [new HtmlWebpackPlugin(
+        {
+            title: '小k',
+            template: 'src/assets/index.html'
+        })],
 
 };

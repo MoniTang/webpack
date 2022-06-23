8d45d4ddbb4c94b069d14df171bc08f44b5f8292
Tang <istangxy@163.com>
2022-06-23 15:41:56 +0800

    import()懒加载

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import x from "./x.js"
 import image from './assets/1.png'
-console.log(image)
 const div = document.getElementById('app')
 
 div.innerHTML = `
 <img src="${image}">
 `
+
+const button = document.createElement('button')
+button.innerText = '懒加载'
+button.onclick = () => {
+    const promise = import('./lazy')
+    promise.then((module) => {
+        const fn = module.default
+        fn()
+    }, () => { })
+}
+div.appendChild(button)
